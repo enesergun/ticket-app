ec98569287969d5a68be5013c39ef525ac5f0da7
Enes Ergün <enesergun1515@gmail.com>
2023-04-01 01:43:48 +0300

    install navigation tab stack & zustand

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import  useBearStore  from "./store/useBearStore";
+
+const Tab = createBottomTabNavigator();
+
+const HomeScreen = () => {
+  const bears = useBearStore((state) => state.bears)
+  return (
+    <View style={styles.container}>
+      <Text>Home! {bears}</Text>
+      <TouchableOpacity onPress={() => useBearStore.getState().increasePopulation()}><Text>bas</Text></TouchableOpacity>
+      <TouchableOpacity onPress={() => useBearStore.getState().removeAllBears()}><Text>sıfırla</Text></TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const SettingsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>SettingsScreen!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
