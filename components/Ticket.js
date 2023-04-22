3003fd3bb04730d6a6ade59d2d3f12681ba1a1ec
Enes Ergün <enesergun1515@gmail.com>
2023-04-22 15:51:44 +0300

    fligh icon animated

--- components/Ticket.js
+++ components/Ticket.js
@@ -1,9 +1,20 @@
-import { Text, View } from "react-native";
-import React, { Component } from "react";
+import { Text, View, Animated } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
 import { styles } from "../style/components/ticketStyle";
 import Icons from "../constants/icons/icons";
 
 const Ticket = ({ title, viewAll, children, alignRight }) => {
+  const translation = useRef(new Animated.Value(50)).current;
+  useEffect(() => {
+   
+    Animated.loop(
+      Animated.timing(translation, {
+        toValue: -50,
+        duration: 5000,
+        useNativeDriver: true,
+      })
+    ).start();
+  }, []);
   return (
     <View style={styles.TicketContainer}>
       <View
@@ -14,17 +25,19 @@ const Ticket = ({ title, viewAll, children, alignRight }) => {
         <Text style={styles.TicketDay}>Dec 24 2022</Text>
       </View>
       <View style={styles.Trip}>
-        <View style={styles.dashedDot}/>
+        <View style={styles.dashedDot} />
         <View style={styles.dot} />
-        <View
+        <Animated.View
+          ref={translation}
           style={[
             {
-              transform: [{ rotate: "90deg" }],
+              transform: [{ rotate: "90deg" }, { translateY: translation }],
+              zIndex: 1,
             },
           ]}
         >
           <Icons name={"FlightIcon"} color={"#FA8D35"} />
-        </View>
+        </Animated.View>
         <View style={styles.dot} />
       </View>
       <View style={[styles.TicketInfoWrapper, styles.TicketınfoRight]}>
